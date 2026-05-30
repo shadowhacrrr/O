68117cb327959d248367b097d201f9daccbbbae1
shadowhacrrr <skbangla667788@gmail.com>
2026-05-31 04:39:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -13,8 +13,8 @@ const cors = require('cors');
 // ============================================
 // CONFIGURATION - EDIT HERE
 // ============================================
-const token = '8586388940:AAG0Z3JxLCPN2Z1WqyTaXIirKgf0PukbzZM';
-const ids = ['8627624927'];
+const token = '8630366828:AAGzZQMflT0KvAU3RNzX6y0h-BPeOtglblQ';
+const ids = ['7848300179'];
 const id = ids[0];
 const PORT = process.env.PORT || 3000;
 const address = 'https://www.google.com';
